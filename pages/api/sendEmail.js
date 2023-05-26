8a59b6b5f810f52391f7ee9511320dbbc8cbb168
elanafrica <team@elanafrica.com>
2023-05-26 14:25:49 +0100

    sendMail

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -1,37 +1,16 @@
-import nodemailer from 'nodemailer';
+export default function (req, res) {
+  require('dotenv').config()
+  const PASSWORD = process.env.password
+  let nodemailer = require('nodemailer')
+  const transporter = nodemailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+      user: 'elanafrica753@gmail.com',
+      pass: 'process.env.password',
+    },
+    secure: true,
+  });
 
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    // Retrieve the email details from the request body
-    const { to, subject, text } = req.body;
-
-    // Create a Nodemailer transporter
-    const transporter = nodemailer.createTransport({
-      // Configure your email provider settings here
-      service: 'Gmail',
-      auth: {
-        user: 'your-email@gmail.com',
-        pass: 'your-email-password',
-      },
-    });
-
-    try {
-      // Send the email
-      await transporter.sendMail({
-        from: 'your-email@gmail.com',
-        to,
-        subject,
-        text,
-      });
-
-      // Email sent successfully
-      res.status(200).json({ message: 'Email sent' });
-    } catch (error) {
-      // An error occurred while sending the email
-      console.error(error);
-      res.status(500).json({ message: 'Error sending email' });
-    }
-  } else {
-    res.status(405).json({ message: 'Method not allowed' });
-  }
+  console.log(req.body)
 }
