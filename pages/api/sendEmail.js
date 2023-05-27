184408507b037e37856f7687becd86a33850dda7
Fortunechinenyem <fortunechinenyem@gmail.com>
2023-05-27 02:02:34 +0100

    update

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -1,16 +1,16 @@
 export default function (req, res) {
-  require('dotenv').config()
-  const PASSWORD = process.env.password
-  let nodemailer = require('nodemailer')
+  require("dotenv").config();
+  const PASSWORD = process.env.password;
+  let nodemailer = require("nodemailer");
   const transporter = nodemailer.createTransport({
     port: 465,
     host: "smtp.gmail.com",
     auth: {
-      user: 'elanafrica753@gmail.com',
-      pass: 'process.env.password',
+      user: "elanafrica753@gmail.com",
+      pass: "process.env.password",
     },
     secure: true,
   });
 
-  console.log(req.body)
+  console.log(req.body);
 }
