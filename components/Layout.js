c3cc4409e2bfa27ab58dd1c340e0126a77cb7ace
Fortunechinenyem <fortunechinenyem@gmail.com>
2023-08-15 07:59:20 +0100

    fix: meta data

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { Inter } from "next/font/google";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 // import Navbar2 from "./Navbar2";
@@ -15,15 +16,28 @@ import Footer from "./Footer";
 //     </>
 //   );
 // }
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Elan Africa",
+  description: "A business growth consultancy Agency",
+};
 
 const Layout = ({ children }) => {
   return (
-    <div>
+    <html lang="en">
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content={metadata.description} />
+        <title>{metadata.title}</title>
+        <style>{inter.styles}</style>
+      </Head>
       <Navbar />
       {/* <Navbar2 /> */}
       <main>{children}</main>
       <Footer />
-    </div>
+    </html>
   );
 };
 
